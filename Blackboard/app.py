c0627f0637252fcf6a45edd090670e73739fbaa3
Duy Truong <duytruong@Duys-MacBook-Pro.local>
2020-03-23 14:54:41 -0500

    update student data w/o mySq

--- Blackboard/app.py
+++ Blackboard/app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from data import Students
+
 
 app = Flask (__name__)
+Students = Students ()
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/students')
+def students():
+    return render_template('students.html', students = Students)
+
+@app.route('/student/<string:id>/')
+def student(id):
+    return render_template('student.html', id=id)
 
 @app.route('/about')
 def about():
---
+++ Blackboard/data.py
@@ -0,0 +1,35 @@
+def Students():
+    students = [
+        {
+            'id' : 1,
+            'name' : 'Duy Truong',
+            'address' : 'Sugar Land, Texas',
+            'year' : 'senior',
+        },
+        {
+                'id' : 2,
+                'name' : 'Student 2',
+                'address' : 'Houston , Texas',
+                'year' : '???',
+        },
+        {
+                'id' : 3,
+                'name' : 'Student 3',
+                'address' : 'Houston , Texas',
+                'year' : '???',
+        },
+        {
+                'id' : 4,
+                'name' : 'Student 4',
+                'address' : 'Houston , Texas',
+                'year' : '???',
+        },
+        {
+                'id' : 5,
+                'name' : 'Student 5',
+                'address' : 'Houston , Texas',
+                'year' : '???',
+        },
+
+    ]
+    return students
